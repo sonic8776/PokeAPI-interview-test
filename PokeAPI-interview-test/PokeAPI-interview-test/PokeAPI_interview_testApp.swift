8a510b43a3c9ef7ad3b33df82c2ad2ad28f57c9d
Judy Tsai <sonic8776@gmail.com>
2024-10-18 20:37:09 +0800

    Create project

---
+++ PokeAPI-interview-test/PokeAPI-interview-test/PokeAPI_interview_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeAPI_interview_testApp.swift
+//  PokeAPI-interview-test
+//
+//  Created by Judy Tsai on 2024/10/18.
+//
+
+import SwiftUI
+
+@main
+struct PokeAPI_interview_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
