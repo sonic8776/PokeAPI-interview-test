d08d1a74c314670b4ec4f24788b59a298adec224
Judy Tsai <sonic8776@gmail.com>
2024-10-20 19:10:47 +0800

    Modify NetworkModule to use Combine

--- PokeAPI-interview-test/PokeAPI-interview-test/NetworkModule/HTTPClient.swift
+++ PokeAPI-interview-test/PokeAPI-interview-test/NetworkModule/HTTPClient.swift
@@ -6,12 +6,27 @@
 //
 
 import Foundation
+import Combine
 
 enum HTTPClientError: Error {
     case networkError
     case cannotFindDataOrResponse
+    case invalidStatusCode(Int)
 }
 
 protocol HTTPClient {
+    func request(with requestType: RequestType) -> AnyPublisher<(Data, HTTPURLResponse), HTTPClientError>
     func request(with requestType: RequestType, completion: @escaping (Result<(Data, HTTPURLResponse), HTTPClientError>) -> Void)
+}
+
+// MARK: - Convenience Extensions
+extension HTTPClient {
+    func requestJSON<T: Decodable>(with requestType: RequestType,
+                                   type: T.Type,
+                                   decoder: JSONDecoder = JSONDecoder()) -> AnyPublisher<T, Error> {
+        return request(with: requestType)
+            .map { (data, _) in data }
+            .decode(type: T.self, decoder: decoder)
+            .eraseToAnyPublisher()
+    }
 }
--- PokeAPI-interview-test/PokeAPI-interview-test/NetworkModule/URLSessionHTTPClient.swift
+++ PokeAPI-interview-test/PokeAPI-interview-test/NetworkModule/URLSessionHTTPClient.swift
@@ -6,8 +6,9 @@
 //
 
 import Foundation
+import Combine
 
-class URLSessionHTTPClient: HTTPClient {
+final class URLSessionHTTPClient: HTTPClient {
     
     let session: URLSession
     
@@ -15,6 +16,29 @@ class URLSessionHTTPClient: HTTPClient {
         self.session = session
     }
     
+    // Using Combine
+    func request(with requestType: RequestType) -> AnyPublisher<(Data, HTTPURLResponse), HTTPClientError> {
+        return session.dataTaskPublisher(for: requestType.urlRequest)
+            .mapError { _ in HTTPClientError.networkError }
+            .flatMap { (data, response) -> AnyPublisher<(Data, HTTPURLResponse), HTTPClientError> in
+                guard let response = response as? HTTPURLResponse else {
+                    return Fail(error: HTTPClientError.cannotFindDataOrResponse)
+                        .eraseToAnyPublisher()
+                }
+                
+                guard response.statusCode == 200 else {
+                    return Fail(error: HTTPClientError.invalidStatusCode(response.statusCode))
+                        .eraseToAnyPublisher()
+                }
+                
+                return Just((data, response))
+                    .setFailureType(to: HTTPClientError.self)
+                    .eraseToAnyPublisher()
+            }
+            .eraseToAnyPublisher()
+    }
+    
+    // Not using Combine
     func request(with requestType: RequestType, completion: @escaping (Result<(Data, HTTPURLResponse), HTTPClientError>) -> ()) {
         session.dataTask(with: requestType.urlRequest) { data, response, error in
             if error != nil {
